8eeab0ba6a33d63457e7084ee7f8cd33e8b2eb9c
Gabriel Taylor Russ <gabriel.taylor.russ@gmail.com>
2020-07-09 07:21:18 -0500

    Version 0.1.1

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule HealthCheckPlug.MixProject do
   def project do
     [
       app: :plug_health,
-      version: "0.1.0",
+      version: "0.1.1",
       elixir: "~> 1.9",
       deps: deps(),
       description: description(),
