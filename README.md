355c5048bccc246f97bc64c55a64389bf5529e68
c4710n <c4710n@users.noreply.github.com>
2022-03-12 17:21:47 +0800

    Bump version

--- README.md
+++ README.md
@@ -9,7 +9,7 @@ Add `plug_health` to your list of dependencies in `mix.exs`:
 ```elixir
 def deps do
   [
-    {:plug_health, "~> 0.1.0"}
+    {:plug_health, "~> 1.0.0"}
   ]
 end
 ```
--- mix.exs
+++ mix.exs
@@ -1,18 +1,21 @@
 defmodule PlugHealth.MixProject do
   use Mix.Project
 
+  @version "1.0.0"
+  @source_url "https://github.com/gabrieltaylor/plug_health"
+  @description "A plug for health check, which can be used for liveness or readiness probes."
+
   def project do
     [
       app: :plug_health,
-      version: "0.1.2",
+      version: @version,
       elixir: "~> 1.9",
       deps: deps(),
-      description: description(),
+      description: @description,
+      homepage_url: @source_url,
       package: package(),
-      source_url: "https://github.com/gabrieltaylor/plug_health",
-      docs: [
-        extras: ~W(README.md)
-      ]
+      aliases: aliases(),
+      docs: docs()
     ]
   end
 
@@ -29,20 +32,33 @@ defmodule PlugHealth.MixProject do
     ]
   end
 
-  defp description do
-    """
-    A plug for health check, which can be used for liveness or readiness probes.
-    """
-  end
-
   defp package do
     [
       files: ~w(lib mix.exs README.md LICENSE),
       maintainers: ["Gabriel Taylor Russ"],
       licenses: ["MIT"],
       links: %{
-        "Github" => "http://github.com/gabrieltaylor/plug_health"
+        "GitHub" => @source_url
       }
+    ]
+  end
+
+  defp aliases do
+    [publish: ["hex.publish", "tag"], tag: &tag_release/1]
+  end
+
+  defp tag_release(_) do
+    Mix.shell().info("Tagging release as #{@version}")
+    System.cmd("git", ["tag", @version])
+    System.cmd("git", ["push", "--tags"])
+  end
+
+  defp docs do
+    [
+      extras: ~W(README.md),
+      main: "readme",
+      source_url: @source_url,
+      source_ref: @version
     ]
   end
 end
